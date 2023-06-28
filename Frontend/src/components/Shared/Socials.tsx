c0202e1b08aa977010d3ce4419cc1be74cd77862
Jiří Malec <jirka.malec98@seznam.cz>
2023-06-28 20:47:03 +0200

    WIP

--- Frontend/src/components/Shared/Socials.tsx
+++ Frontend/src/components/Shared/Socials.tsx
@@ -1,24 +1,19 @@
 import { FC, useState } from "react";
 import { EventModel, ModalModel } from "../../utils/Types";
 
-
-const Socials => {
+const Socials = () => {
 	return (
-		<div class="socials">
-						<a href="#">
-							<i class="fab fa-linkedin fa-lg"></i>
-						</a>
-						<a href="#">
-							<i class="fab fa-github fa-lg"></i>
-						</a>
-						<a
-							href="./../public/files/cv.pdf"
-							download
-							target="_blank"
-						>
-							<span class="text-xxl">CV</span>
-						</a>
-					</div>
+		<div className="socials">
+			<a href="https://www.linkedin.com/in/jiri-malec/">
+				<i className="fab fa-linkedin"></i>
+			</a>
+			<a href="#">
+				<i className="fab fa-github"></i>
+			</a>
+			<a href="./../public/files/cv.pdf" download target="_blank">
+				<span className="text-xl">CV</span>
+			</a>
+		</div>
 	);
 };
 
