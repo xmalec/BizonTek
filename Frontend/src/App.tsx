422134f219fcbaf979d2efe8e02982486f34121c
Jiří Malec <jirka.malec98@seznam.cz>
2023-07-27 19:04:19 +0200

    next fe done

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.scss";
 import "./styles/fontawesome/css/all.css";
+import "./styles/fontawesome";
 import { BrowserRouter } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import GA4 from "./GA4";
