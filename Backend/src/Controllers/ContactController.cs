4821d630cd812d072f214217903c90713660ed60
Jiří Malec <jiri.malec@bluesoft.cz>
2024-10-03 18:18:29 +0200

    init be

---
+++ Backend/src/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Contact Controller");
+        }
+
+        [HttpPost]
+        public IActionResult SendEmail([FromBody] ContactFormModel contact)
+        {
+            if (contact == null)
+            {
+                return BadRequest();
+            }
+
+            // Save contact to database
+            return Ok();
+        }
+    }
+}
