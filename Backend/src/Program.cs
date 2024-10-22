8bc912ea3593f6af9ead6eb9cc4cc24ca8c7b939
Jiří Malec <jirka.malec98@seznam.cz>
2024-10-22 20:15:48 +0200

    adding publish profiles, setting health checks

--- Backend/src/Program.cs
+++ Backend/src/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
+builder.Services.AddHealthChecks();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -11,6 +11,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseHealthChecks("/status");
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
