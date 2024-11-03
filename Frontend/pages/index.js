f6f8f1c0a042e8de18302e91856d5c57c48a68a5
Jiří Malec <jirka.malec98@seznam.cz>
2024-11-03 13:07:18 +0100

    fix

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -6,7 +6,7 @@ import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
-    this.document.documentElement.lang = "cs";
+    document.documentElement.lang = "cs";
   }, []);
   return (
     <>
