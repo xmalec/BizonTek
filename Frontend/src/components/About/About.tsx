3735ad3e80dddd7ae847a35f0ef88b317a20b64a
Jiří Malec <jiri.malec@bluesoft.cz>
2025-01-04 18:27:41 +0100

    adding title to about section

--- Frontend/src/components/About/About.tsx
+++ Frontend/src/components/About/About.tsx
@@ -4,6 +4,7 @@ const About = () => {
   return (
     <div className="about">
       <span className="link-anchor" id="o-mne"></span>
+      <h3 className="h3 m-0 about__title">Zkušenosti a vzdělání</h3>
       <div className="timeline-container section">
         <div className="line-container">
           <div className="top-title">Současnost</div>
